6c9fc7006e944bedf03ba69dd5c01405752d31ff
Arthur_Wu <935391095@qq.com>
2023-06-26 15:27:34 +0800

    Add files via upload

--- priority_queue.h
+++ priority_queue.h
@@ -1,5 +1,5 @@
 //
-// Created by 吴映鹏 on 2023/6/12.
+// Created by 93539 on 2023/6/24.
 //
 
 #ifndef PRIORITY_QUEUE_PRIORITY_QUEUE_H
@@ -11,7 +11,7 @@ namespace wyp {
     template<class T>
     class less {
     public:
-        bool operator()(const T &left, const T &right) const {
+        bool operator()(const T &left, const T &right) {
             return left < right;
         }
     };
@@ -19,7 +19,7 @@ namespace wyp {
     template<class T>
     class greater {
     public:
-        bool operator()(const T &left, const T &right) const {
+        bool operator()(const T &left, const T &right) {
             return left > right;
         }
     };
@@ -27,23 +27,32 @@ namespace wyp {
     template<class T, class Container = std::vector<T>, class Compare = less<T>>
     class priority_queue {
     public:
+        template<class InputIterator>
+        priority_queue(InputIterator first, InputIterator last) {
+            _con(first, last);
+        }
+
         priority_queue() {
 
         }
 
-        template<class InputIterator>
-        priority_queue(InputIterator first, InputIterator last)
-                : _con(first, last) {
-            ////建堆
-            for (int i = (_con.size() - 1 - 1) / 2; i >= 0; --i) {
-                adjust_down(i);
-            }
+        bool empty() const {
+            return _con.empty();
         }
 
-        void adjust_up(size_t child) {
+        std::size_t size() const {
+            return _con.size();
+        }
+
+        const T &top() const {
+            return _con.front();
+        }
+
+        ////建大堆
+        void adjust_up(std::size_t child) {
             Compare com;
-            size_t parent = (child - 1) / 2;
-            while (child > 0) {
+            std::size_t parent = (child - 1) / 2;
+            while (child) {
                 if (com(_con[parent], _con[child])) {
                     std::swap(_con[child], _con[parent]);
                     child = parent;
@@ -54,20 +63,21 @@ namespace wyp {
             }
         }
 
-        void push(const T &x) {
-            _con.push_back(x);
+        void push(const T &value) {
+            _con.push_back(value);
             adjust_up(_con.size() - 1);
         }
 
-        void adjust_down(size_t parent) {
+        ////默认建大堆
+        void adjust_down(std::size_t parent) {
             Compare com;
-            size_t child = parent * 2 + 1;
-            while (child < _con.size()) {
+            std::size_t child = parent * 2 + 1;
+            while (child < _con.size()) {        //_con[child] < _con[child + 1]
                 if (child + 1 < _con.size() && com(_con[child], _con[child + 1])) {
                     ++child;
                 }
                 if (com(_con[parent], _con[child])) {
-                    std::swap(_con[child], _con[parent]);
+                    std::swap(_con[parent], _con[child]);
                     parent = child;
                     child = parent * 2 + 1;
                 } else {
@@ -79,19 +89,10 @@ namespace wyp {
         void pop() {
             std::swap(_con[0], _con[_con.size() - 1]);
             _con.pop_back();
+//            for (int i = (_con.size() - 1 - 1) / 2; i >= 0; --i) {
+//                adjust_down(i);
+//            }
             adjust_down(0);
-        }
-
-        const T &top() const {
-            return _con[0];
-        }
-
-        bool empty() const {
-            return _con.empty();
-        }
-
-        size_t size() const {
-            return _con.size();
         }
 
     private:
